cc2efbfda3f42d2ed7567d509225b12c6448e938
Freddy <cgetsfred@gmail.com>
2016-11-20 22:55:11 -0600

    Added tests for EnumType

---
+++ __test__/EnumType.test.js
@@ -0,0 +1,46 @@
+import {Enum} from '../lib/mws';
+
+describe('Enum', () => {
+  it('should not be null', () => {
+    expect(Enum).toBeDefined();
+  });
+
+  it('#all should contain all choices', () => {
+    const choices = ['Basic', 'Detailed'];
+    const e = new Enum(choices);
+
+    expect(e.all()).toEqual(choices);
+  });
+
+  it('#ctor should disable all choices', () => {
+    const choices = ['Basic', 'Detailed'];
+    const e = new Enum(choices);
+
+    expect(e.values()).toEqual([]);
+  });
+
+  it('#enable should enable specified choices', () => {
+    const choices = ['Basic', 'Detailed'];
+    const e = new Enum(choices);
+    e.enable(choices[0]);
+    expect(e.values()).toEqual([choices[0]]);
+  });
+
+  it('#disable should enable specified choices', () => {
+    const choices = ['Basic', 'Detailed'];
+    const e = new Enum(choices);
+    e.enable(choices[0], choices[1]);
+    e.disable(choices[0]);
+
+    expect(e.values()).toEqual([choices[1]]);
+  });
+
+  it('#toggle should enable specified choices', () => {
+    const choices = ['Basic', 'Detailed'];
+    const e = new Enum(choices);
+    e.toggle(choices[1]);
+
+    expect(e.values()).toEqual([choices[1]]);
+  });
+
+});
