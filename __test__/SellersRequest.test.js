4069c7ab9060a510a59a4b1a7580c7523ed6fe4d
Freddy <cgetsfred@gmail.com>
2016-11-20 21:19:22 -0600

    Added SellerRequest tests

--- __test__/SellersRequest.test.js
+++ __test__/SellersRequest.test.js
@@ -1,3 +1,36 @@
-/**
- * Created by Freddy on 11/20/16.
- */
+import {Sellers} from '../lib/mws'
+
+describe('SellersRequest', () => {
+
+  it('should not be null', () => {
+    expect(Sellers).toBeDefined();
+  });
+
+  it('should have the expected calls', () => {
+    const expectedCalls = ['GetServiceStatus', 'ListMarketplaceParticipations', 'ListMarketplaceParticipationsByNextToken'];
+    const actualKeys = Object.keys(Sellers.requests);
+
+    expect(actualKeys).toEqual(expectedCalls);
+  });
+
+  test('GetServiceStatus should populate properties', ()=>{
+    const request = Sellers.requests.GetServiceStatus();
+    expect(request).toMatchSnapshot();
+  });
+
+  test('ListMarketplaceParticipations should populate properties', ()=>{
+    const request = Sellers.requests.ListMarketplaceParticipations();
+    expect(request).toMatchSnapshot();
+  });
+
+  test('ListMarketplaceParticipationsByNextToken should populate properties', ()=>{
+    const request = Sellers.requests.ListMarketplaceParticipationsByNextToken();
+    expect(request).toMatchSnapshot();
+  });
+
+  test('types should return expected values', ()=> {
+    const types = Sellers.types;
+    expect(types).toMatchSnapshot();
+  });
+
+});
