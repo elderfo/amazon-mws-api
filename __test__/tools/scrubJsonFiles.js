84eec69209f6f7f021f5585daa0932491a92dca0
Freddy <cgetsfred@gmail.com>
2016-11-20 18:10:53 -0600

    formatted file

--- __test__/tools/scrubJsonFiles.js
+++ __test__/tools/scrubJsonFiles.js
@@ -8,36 +8,35 @@ import ListOrders from '../data/ListOrders.json';
 
 export function scrubListFile() {
 
-    const listOrdersFile = path.join(__dirname, '../data/ListOrders.json');
+  const listOrdersFile = path.join(__dirname, '../data/ListOrders.json');
 
-    console.log(`Scrubbing file: ${listOrdersFile}`.yellow);
+  console.log(`Scrubbing file: ${listOrdersFile}`.yellow);
 
-    const orders = ListOrders.ListOrdersResponse.ListOrdersResult.Orders.Order;
-    orders.forEach(order => {
-        const name = uuid.v4();
+  const orders = ListOrders.ListOrdersResponse.ListOrdersResult.Orders.Order;
+  orders.forEach(order => {
+    const name = uuid.v4();
 
-        order.BuyerEmail = uuid.v4();
-        order.AmazonOrderId = uuid.v4();
-        order.BuyerName = name;
-        order.MarketplaceId = uuid.v4();
+    order.BuyerEmail = uuid.v4();
+    order.AmazonOrderId = uuid.v4();
+    order.BuyerName = name;
+    order.MarketplaceId = uuid.v4();
 
-        if ( order.ShippingAddress)
-        {
-            Object.keys(order.ShippingAddress).forEach((key) => {
-                order.ShippingAddress[key] = uuid.v4();
-            });
+    if (order.ShippingAddress) {
+      Object.keys(order.ShippingAddress).forEach((key) => {
+        order.ShippingAddress[key] = uuid.v4();
+      });
 
-            order.ShippingAddress.Name = name;
-        }
-    });
+      order.ShippingAddress.Name = name;
+    }
+  });
 
-    ListOrders.ListOrdersResponse.ListOrdersResult.NextToken = uuid.v4();
+  ListOrders.ListOrdersResponse.ListOrdersResult.NextToken = uuid.v4();
 
-    fs.writeFile(listOrdersFile, JSON.stringify(ListOrders, null, 2), 'utf-8', err => {
-        if (err) {
-            console.log(`Failed to save file: ${err}`.red);
-        }
-    });
+  fs.writeFile(listOrdersFile, JSON.stringify(ListOrders, null, 2), 'utf-8', err => {
+    if (err) {
+      console.log(`Failed to save file: ${err}`.red);
+    }
+  });
 }
 
 scrubListFile();
